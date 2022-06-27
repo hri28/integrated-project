bca1496b6945cb8a1fd4c14faa64d5ae6015181c
Hrishita <hrishita282001@gmail.com>
2022-06-27 18:12:37 +0530

    adding project

---
+++ src/Landing.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+import './Landing.css'
+
+
+const Landing = () => {
+  return (
+    <div className='container'>
+        <div className='title-container'>
+        <h1 className='landing-title'>Moyo</h1>
+        <h3 className='landing-subheader'>Meditate and Connect.</h3>
+        <h3 className='landing-para'>Start a focus session with ambient sounds and practise meditation</h3>
+        <Link to='/timer'><button className='landing-btn' type='submit'>Start</button></Link>
+        </div>
+    </div>
+  )
+}
+
+export default Landing
